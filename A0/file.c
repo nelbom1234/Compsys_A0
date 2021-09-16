02b5fec4b22e78a02564fececf54215641a8a0ca
nelbom1234 <nelbom1234@gmail.com>
2021-09-16 16:29:40 +0200

    existence is pain

--- A0/file.c
+++ A0/file.c
@@ -5,18 +5,20 @@
 #include <assert.h>
 
 enum file_type {
-    DATA,
+    UNDEFINED,
     EMPTY,
+    DATA,
     ASCII,
     ISO_8859_1,
     UTF8
 };
 
 const char * const FILE_TYPE_STRINGS[] = {
-    "data",
+    "undefined",
     "empty",
+    "data",
     "ascii",
-    "ise-8859-1",
+    "iso-8859-1",
     "utf8"
 };
 
@@ -24,15 +26,80 @@ enum file_type fileType(FILE *f) {
     fseek(f, 0, SEEK_END);
     if (ftell(f) == 0)
         return EMPTY;
-    
-    else
-        for(;;){
-            enum file_type first_char;
-            
+    fseek(f,0, SEEK_SET);
+    enum file_type first_char = UNDEFINED;
+    unsigned char c;
+    while(fgetc(f) != EOF) {    
+        fread(&c, sizeof(char), 1, f);
+
+        if ((c >= 7 && c <= 13) || c == 27 || (c >= 32 && c <= 126)) {
+            if (first_char == ASCII || first_char == UNDEFINED) {
+                first_char = ASCII;
+                continue;
         }
-        
-        return DATA;
+            else
+                return DATA;
+        }
+        else if(c >= 160){
+            if (first_char == ISO_8859_1 || first_char == UNDEFINED || first_char == ASCII) {
+                first_char = ISO_8859_1;
+                continue;
+            }
+            else
+                return DATA;
+        }
+        else if((c & 0x80) == 0x0) {
+            if (first_char == UTF8 || first_char == UNDEFINED || first_char == ISO_8859_1 || first_char == ASCII) {
+                first_char = UTF8;
+                continue;
+            }
+            else
+                return DATA;
+        }
+        else if((c & 0xe0) == 0xc0) {
+            char byte;
+            fread(&byte, sizeof(char), 1, f);
+            if((byte & 0xc0) == 0x80) {
+                if (first_char == UTF8 || first_char == UNDEFINED || first_char == ISO_8859_1 || first_char == ASCII) {
+                first_char = UTF8;
+                continue;
+            }
+            else
+                return DATA;
+            }
+        }
+        else if((c & 0xf0) == 0xe0) {
+            char bytearr[2];
+            fread(&bytearr[0],sizeof(char),1,f);
+            fread(&bytearr[1],sizeof(char),1,f);
+            if((bytearr[0] & 0xc0) == 0x80 && (bytearr[1] & 0xc0) == 0x80) {
+                if (first_char == UTF8 || first_char == UNDEFINED || first_char == ISO_8859_1 || first_char == ASCII) {
+                first_char = UTF8;
+                continue;
+            }
+            else
+                return DATA;
+            }
+
+        }
+        else if ((c & 0xf8) == 0xf0) {
+            char bytearr[3];
+            fread(&bytearr[0],sizeof(char),1,f);
+            fread(&bytearr[1],sizeof(char),1,f);
+            fread(&bytearr[2],sizeof(char),1,f);
+            if((bytearr[0] & 0xc0) == 0x80 && (bytearr[1] & 0xc0) == 0x80 && (bytearr[2] & 0xc0) == 0x80) {
+                if (first_char == UTF8 || first_char == UNDEFINED || first_char == ISO_8859_1 || first_char == ASCII) {
+                first_char = UTF8;
+                continue;
+            }
+            else
+                return DATA;
+            }
+        }
+    }
+    return first_char;
 }
+
 
 int main(int argc, char* argv[]) {
     assert(argc == 2);
